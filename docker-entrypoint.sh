9ae5459b2eaf1d62065f344278f613065f40c4a1
Stéphane Gully <stephane.gully@gmail.com>
2018-08-22 11:29:07 +0200

    ignore the .git folders and clone the translators repository at first container startup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # will be deprecated when https://github.com/zotero/translation-server-v2/issues/1 will be released
-echo "Updating zotero translators"
+if [ ! -d /app/modules/translators/ ]; then
+  echo "-> Clonning zotero translators (initialization step)"
+  git clone https://github.com/zotero/translators /app/modules/translators/
+fi
+echo "-> Updating zotero translators"
 cd /app/modules/translators/
 git pull origin master
 
