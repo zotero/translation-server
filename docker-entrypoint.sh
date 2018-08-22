47ebf4b96ff3ce85d1f881865ac9460cb6e7a22d
Stéphane Gully <stephane.gully@gmail.com>
2018-08-22 11:31:48 +0200

    Clonning => Cloning

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 # will be deprecated when https://github.com/zotero/translation-server-v2/issues/1 will be released
 if [ ! -d /app/modules/translators/ ]; then
-  echo "-> Clonning zotero translators (initialization step)"
+  echo "-> Cloning zotero translators (initialization step)"
   git clone https://github.com/zotero/translators /app/modules/translators/
 fi
 echo "-> Updating zotero translators"
