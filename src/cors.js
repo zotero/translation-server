1d15419e69572d95150ebd97923c9d114a9c2f6c
Dan Stillman <dstillman@zotero.org>
2018-10-27 01:42:00 -0400

    Return CORS headers on errors
    Koa normally strips headers on errors

--- src/cors.js
+++ src/cors.js
@@ -1,15 +1,26 @@
 const config = require('config');
 
 module.exports = async (ctx, next) => {
+	headersSet = {};
+	function set(key, value) {
+		ctx.set(key, value);
+		headersSet[key] = value;
+	}
 	if (ctx.headers.origin) {
 		let allowedOrigins = config.get('allowedOrigins').filter(x => x);
 		let allAllowed = allowedOrigins.includes('*');
 		if (allAllowed || allowedOrigins.includes(ctx.headers.origin)) {
-			ctx.set("Access-Control-Allow-Origin", allAllowed ? '*' : ctx.headers.origin);
-			ctx.set("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-			ctx.set("Access-Control-Allow-Headers", "Content-Type");
-			ctx.set("Access-Control-Expose-Headers", "Link");
+			set("Access-Control-Allow-Origin", allAllowed ? '*' : ctx.headers.origin);
+			set("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+			set("Access-Control-Allow-Headers", "Content-Type");
+			set("Access-Control-Expose-Headers", "Link");
 		}
 	}
-	await next();
+	try {
+		await next();
+	}
+	catch (e) {
+        e.headers = Object.assign({}, e.headers, headersSet);
+        throw e;
+    }
 };
