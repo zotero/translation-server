836cec3ca6a48886fa65a1c6b1d4fc9a66841761
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2019-03-08 21:58:29 +0100

    remove normalization, add url tag sorting

--- src/importEndpoint.js
+++ src/importEndpoint.js
@@ -26,124 +26,11 @@
 const config = require('config');
 const Translate = require('./translation/translate');
 
-const fields = {
-	valid: [
-		"itemType",
-		"dateAdded",
-		"dateModified",
-		"tags",
-		"attachments",
-		"notes",
-		"collections",
-		"url",
-		"rights",
-		"series",
-		"volume",
-		"edition",
-		"place",
-		"publisher",
-		"ISBN",
-		"date",
-		"callNumber",
-		"archiveLocation",
-		"extra",
-		"accessDate",
-		"seriesNumber",
-		"numberOfVolumes",
-		"libraryCatalog",
-		"language",
-		"abstractNote",
-		"title",
-		"shortTitle",
-		"numPages",
-		"archive",
-		"pages",
-		"publicationTitle",
-		"issue",
-		"ISSN",
-		"journalAbbreviation",
-		"DOI",
-		"seriesTitle",
-		"seriesText",
-		"section",
-		"type",
-		"medium",
-		"runningTime",
-		"artworkSize",
-		"number",
-		"code",
-		"session",
-		"legislativeBody",
-		"history",
-		"reporter",
-		"court",
-		"committee",
-		"assignee",
-		"priorityNumbers",
-		"references",
-		"legalStatus",
-		"country",
-		"applicationNumber",
-		"issuingAuthority",
-		"filingDate",
-		"codeNumber",
-		"scale",
-		"meetingName",
-		"versionNumber",
-		"system",
-		"programmingLanguage",
-		"conferenceName"
-	],
-
-	aliasOf: {
-		bookTitle: "publicationTitle",
-		thesisType: "type",
-		university: "publisher",
-		letterType: "type",
-		manuscriptType: "type",
-		interviewMedium: "medium",
-		distributor: "publisher",
-		videoRecordingFormat: "medium",
-		genre: "type",
-		artworkMedium: "medium",
-		websiteType: "type",
-		websiteTitle: "publicationTitle",
-		institution: "publisher",
-		reportType: "type",
-		reportNumber: "number",
-		billNumber: "number",
-		codeVolume: "volume",
-		codePages: "pages",
-		dateDecided: "date",
-		reporterVolume: "volume",
-		firstPage: "pages",
-		caseName: "title",
-		docketNumber: "number",
-		documentNumber: "number",
-		patentNumber: "number",
-		issueDate: "date",
-		dateEnacted: "date",
-		publicLawNumber: "number",
-		nameOfAct: "title",
-		subject: "title",
-		mapType: "type",
-		blogTitle: "publicationTitle",
-		postType: "type",
-		forumTitle: "publicationTitle",
-		audioRecordingFormat: "medium",
-		label: "publisher",
-		presentationType: "type",
-		studio: "publisher",
-		network: "publisher",
-		episodeNumber: "number",
-		programTitle: "publicationTitle",
-		audioFileType: "medium",
-		company: "publisher",
-		proceedingsTitle: "publicationTitle",
-		encyclopediaTitle: "publicationTitle",
-		dictionaryTitle: "publicationTitle"
-	}
-};
+function tagValue(tag) {
+	if (typeof tag.tag === 'string') return tag.tag;
+	if (typeof tag.tag.value === 'string') return tag.tag.value;
+	throw new Error(`Unexpected tag format ${JSON.stringify(tag)}`);
+}
 
 class ImportEndpoint {
 	async handle(ctx, _next) {
@@ -175,27 +62,12 @@ class ImportEndpoint {
 		for (const note of (item.notes || [])) {
 			this.normalizeTags(note);
 		}
-
-		for (const [field, baseField] of Object.entries(fields.aliasOf)) {
-			if (typeof item[field] === 'string') {
-				item[baseField] = item[field];
-				delete item[field];
-			}
-		}
-
-		/*
-		for (const [field, value] of Object.entries(item)) {
-			if (['string', 'number'].includes(typeof value) && !fields.valid.includes(field)) {
-				throw new Error(`Invalid field ${item.itemType}.${field}`);
-			}
-		}
-		*/
 	}
 
 	normalizeTags(item) {
 		if (item.tags) item.tags = item.tags.map(tag => (typeof tag === 'string') ? { tag } : tag);
 		if (item.tags && item.tags.length === 0) delete item.tags;
-		if (item.tags) item.tags.sort((a, b) => a.tag.localeCompare(b.tag));
+		if (item.tags) item.tags.sort((a, b) => tagValue(a).localeCompare(tagValue(b)));
 	}
 }
 
