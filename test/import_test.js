083f239cd0a13ebe84a9bacd921459da953ca9bb
Emiliano Heyns <retorquere@users.noreply.github.com>
2019-04-01 21:41:57 +0200

    Import tests: normalize some fields, compare order (#100)

--- test/import_test.js
+++ test/import_test.js
@@ -3,6 +3,10 @@
 const fs = require('fs');
 const path = require('path');
 
+function flatten(array) {
+	return [].concat.apply([], array);
+}
+
 function addTest(name, translatorID, input, expected) {
 	it(`should import ${name}`, async function () {
 		const response = await request()
@@ -13,7 +17,42 @@ function addTest(name, translatorID, input, expected) {
 
 		assert.equal(response.headers['zotero-translator-id'], translatorID);
 
-		assert.deepEqual(response.body, expected);
+		const items = {
+			expected: flatten(expected.map(item => Zotero.Utilities.itemToAPIJSON(item))), // normalize and flatten results
+			found: response.body,
+		};
+
+		for (const ef of ['expected', 'found']) {
+			for (const item of items[ef]) {
+				// inline attached notes -- notes are exported as separate items with a parentItem, but the parentItem is going to be different every time the test is ran
+				if (!item.notes) {
+					item.notes = items[ef]
+						.filter(note => (note.itemType === 'note' && note.parentItem === item.key))
+						.map(note => ({ note: note.note, itemType: note.itemType}));
+				}
+			}
+			// remove notes now inlined
+			items[ef] = items[ef].filter(item => (item.itemType !== 'note' || !item.parentItem));
+
+			for (const item of items[ef]) {
+				// remove purely administrative fields -- must be done in 2nd run because the key/parentItem are required in the 1st run for inlining notes
+				delete item.key;
+				delete item.version;
+				delete item.parentItem;
+
+				// remove empty array to simplify comparison
+				if (Array.isArray(item.tags) && !item.tags.length) {
+					delete item.tags;
+				}
+
+				// sort tags for stable comparison
+				if (item.tags) {
+					item.tags.sort((a, b) => `${a.type}::${a.tag}`.localeCompare(`${b.type}::${b.tag}`));
+				}
+			}
+		}
+
+		assert.deepEqual(items.expected, items.found);
 	});
 }
 
@@ -36,7 +75,7 @@ function parseJSON(name, json, source) {
 	return null;
 }
 
-describe.skip("/import", function () {
+(process.env.IMPORT_TESTS ? describe : describe.skip)("/import", function () {
 	const translators = path.join(__dirname, '../modules/translators');
 	for (const translator of fs.readdirSync(translators)) {
 		if (!translator.endsWith('.js')) continue;
