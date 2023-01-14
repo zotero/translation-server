e7e0b3bebe0f7c45361d55fbfa718db4af159c76
Dan Stillman <dstillman@zotero.org>
2023-01-13 21:44:08 -0500

    Stop at hash on DOI detection in URL
    https://github.com/zotero/translators/issues/2964#issuecomment-1381832672

--- src/webSession.js
+++ src/webSession.js
@@ -464,8 +464,8 @@ WebSession.prototype.deproxifyURL = function (url) {
 WebSession.prototype.cleanDOIFromURL = function (url) {
 	let doi = Zotero.Utilities.cleanDOI(decodeURIComponent(url));
 	if (doi) {
-		// Stop at query string or ampersand
-		doi = doi.replace(/[?&].*/, '');
+		// Stop at query string, ampersand, or hash
+		doi = doi.replace(/[?&#].*/, '');
 	}
 	return doi || null;
 };
