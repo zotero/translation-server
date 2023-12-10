59369c6e71311186f2685808fc3a733549d862bb
Dan Stillman <dstillman@zotero.org>
2023-12-10 05:37:41 -0500

    Increase text/plain POST limit to 5 MB as well

--- src/server.js
+++ src/server.js
@@ -58,7 +58,13 @@ app.use(function (ctx, next) {
 	return next();
 });
 app.use(cors);
-app.use(bodyParser({ enableTypes: ['text', 'json'], jsonLimit: '5mb' }));
+app.use(
+	bodyParser({
+		enableTypes: ['text', 'json'],
+		jsonLimit: '5mb',
+		textLimit: '5mb',
+	})
+);
 app.use(_.post('/web', WebEndpoint.handle.bind(WebEndpoint)));
 app.use(_.post('/search', SearchEndpoint.handle.bind(SearchEndpoint)));
 app.use(_.post('/export', ExportEndpoint.handle.bind(ExportEndpoint)));
