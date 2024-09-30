0747682b753ace37fde11947d1105b4fa5d4e491
JJJHolscher <jochem.holscher@gmail.com>
2024-09-30 17:57:44 +0200

    functioning flake that launches the server

--- flake.nix
+++ flake.nix
@@ -10,9 +10,62 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        packages.default = pkgs.callPackage ./default.nix {};
+
+        # Translation Server Version
+        translationServerVersion = "c4f03e78e1c50dc61a2b8e5a452284581d9ad0e3"; # Specific commit for reproducibility
+       
+        translate = pkgs.fetchFromGitHub {
+          owner = "zotero";
+          repo = "translate";
+          rev = "master";              # Specify the commit or branch
+          sha256 = "sha256-dzkMnGEcFtJ3S10LtLVpq4d5O4f9+Jk12lrXhRegtPk=";
+        };
+        translators = pkgs.fetchFromGitHub {
+          owner = "zotero";
+          repo = "translators";
+          rev = "master";              # Specify the commit or branch
+          sha256 = "sha256-EH0DU/06pleNKv1A2/VIu+KZ85cpbRGSt3hqXUaEoLI=";
+        };
+        utilities = pkgs.fetchFromGitHub {
+          owner = "zotero";
+          repo = "utilities";
+          rev = "master";              # Specify the commit or branch
+          sha256 = "sha256-Pr6Htc1CdNubcZSt3ASwAYagJdPAbdA1a9RXyiqZSJY=";
+        };
+        zotero-schema = pkgs.fetchFromGitHub {
+          owner = "zotero";
+          repo = "zotero-schema";
+          rev = "master";              # Specify the commit or branch
+          sha256 = "sha256-P+4MXUD3ip3DceyUqMh+hgmHwaJqF0jUuuncliFdoYE=";
+        };
+      in {
+        packages.default = pkgs.buildNpmPackage rec {
+          pname = "zotero-translation-server";
+          version = translationServerVersion;
+         
+          src = ./.;
+          npmDepsHash = "sha256-JHoBxUybs1GGRxEVG5GgX2mOCplTgR5dcPjnR42SEbY=";
+          makeCacheWritable = true;
+         
+          buildPhase = ''
+            cp -r ${translate} modules/translate
+            cp -r ${translators} modules/translators
+            cp -r ${utilities} modules/utilities
+            cp -r ${zotero-schema} modules/zotero-schema
+          '';
+         
+          packageJson = "${src}/package.json";
+         
+          # Make the main script executable
+          executable = true;
+         
+          meta = with pkgs.lib; {
+            description = "Zotero Translation Server";
+            homepage = "https://github.com/zotero/translation-server";
+            license = licenses.mit;
+            platforms = platforms.all;
+          };
+        };
         apps.default = flake-utils.lib.mkApp {
           drv = self.packages.${system}.default;
           checkPhase = "true";
